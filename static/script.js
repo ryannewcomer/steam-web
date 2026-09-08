59d6441898d1e1c339438e97a23d542c028f169e
Zx <draem070@gmail.com>
2026-09-08 21:16:43 +0100

    added a grid system

--- static/script.js
+++ static/script.js
@@ -1,6 +1,5 @@
 // select the main div tag
-const gameNameId = document.getElementById("game-name");
-const playTimeId = document.getElementById("play-time");
+const main = document.getElementById("main");
 // use a loop to automaticly add rows and cal from json
 async function populate() {
   const URL = "/api/games";
@@ -24,22 +23,28 @@ async function populate() {
 
 function populateGames(obj) {
   for (const gamesName of obj) {
-    const col1 = document.createElement("div");
-    col1.className = "col";
-    const col2 = document.createElement("div");
-    col2.className = "col";
+    const cell = document.createElement("div");
+    cell.className = "cell";
+
+    cell.style.width = "100px";
+    cell.style.height = "80px";
+    cell.style.backgroundColor = "#c7c8dd";
+    cell.style.borderRadius = "3px";
+    cell.style.margin = "5px";
+    cell.style.boxShadow = "5px #00000049";
 
     col1.textContent = gamesName.name;
     col2.textContent = Math.round(gamesName.playtime_forever / 60);
 
-    gameNameId.appendChild(col1);
-    playTimeId.appendChild(col2);
+    cell.appendChild(col1);
+    cell.appendChild(col2);
   }
 }
 
 function populateName(obj) {
   const header = document.querySelector(".username");
   header.textContent = obj.name;
+  document.title = obj.name + " - Steam Web";
 }
 
 populate();
